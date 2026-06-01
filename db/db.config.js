c7a800a236246b68f6479e33f96d6d6a888c62cd
EyobaGit <juu.shashe@gmail.com>
2026-06-01 23:50:59 +0300

    postgress4 commit

--- db/db.config.js
+++ db/db.config.js
@@ -1,16 +1,14 @@
 // import mysql from 'mysql2/promise';
 // *****************Postgress sql*********
-
+import { pool } from "pg";
 
 // const { Pool } = pg;
 
 const db = new Pool({
-  connectionString: process.env.DB_URL,
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false,
   },
 });
-
-
 
 export default db;
